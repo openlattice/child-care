c593472b0d88a0abfbbf68c4414fcdb60cb27af4
Solomon Tang <solomoncztang@gmail.com>
2020-04-09 22:10:48 -0700

    Add SelectedProviderMarker

---
+++ src/containers/location/providers/SelectedProviderMarker.js
@@ -0,0 +1,31 @@
+// @flow
+import React from 'react';
+
+import { faMapMarkerAlt } from '@fortawesome/pro-solid-svg-icons';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { Map } from 'immutable';
+import { Colors } from 'lattice-ui-kit';
+import { Marker } from 'react-mapbox-gl';
+
+import { getCoordinates } from '../../map/MapUtils';
+
+const { PURPLES } = Colors;
+
+type Props = {
+  provider :Map;
+};
+
+const SelectedProviderMarker = ({ provider } :Props) => {
+  if (!Map.isMap(provider)) return null;
+  const coordinates = getCoordinates(provider);
+  return (
+    <Marker
+        anchor="bottom"
+        coordinates={coordinates}
+        style={{ zIndex: 0 }}>
+      <FontAwesomeIcon icon={faMapMarkerAlt} color={PURPLES[1]} size="3x" />
+    </Marker>
+  );
+};
+
+export default SelectedProviderMarker;
