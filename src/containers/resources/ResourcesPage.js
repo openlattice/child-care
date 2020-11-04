0fc5a2f39db90d48c11df13580ca6b11dfe992cf
Todd Bergman <todd@openlattice.com>
2020-11-04 15:35:39 -0800

    updated resources page to include styled links

--- src/containers/resources/ResourcesPage.js
+++ src/containers/resources/ResourcesPage.js
@@ -6,16 +6,33 @@ import styled from 'styled-components';
 import { connect } from 'react-redux';
 import { Colors, Typography } from 'lattice-ui-kit';
 
+import { RESOURCES, SUBTITLES } from '../../utils/constants/ResourcesConstants';
+import { trackLinkClick } from '../../utils/AnalyticsUtils';
 import { STATE } from '../../utils/constants/StateConstants';
 import { LABELS } from '../../utils/constants/Labels';
 import { getRenderTextFn } from '../../utils/AppUtils';
-import { ContentOuterWrapper, ContentWrapper, TextLink } from '../../components/layout';
+import { ContentOuterWrapper, ContentWrapper } from '../../components/layout';
 
-const { NEUTRAL } = Colors;
+const { NEUTRAL, PURPLE } = Colors;
+
+const {
+  CALIFORNIA_RESOURCE_AND_REFERRAL_NETWORK,
+  CALIFORNIA_COVID19_CHILDCARE_RESPONSE,
+  CALIFORNIA_PARENT_AND_FAMILY_RESOURCES,
+  FAMILY_CHILD_CARE_HOME_LICENSING_INFORMATION,
+} = SUBTITLES;
 
 const Wrapper = styled(ContentWrapper)`
   padding: 30px !important;
   background-color: white;
+
+  a {
+    color: ${PURPLE.P300};
+  }
+
+  h6 {
+    margin: 20px 0 5px;
+  }
 `;
 
 const Description = styled(Typography)`
@@ -26,28 +43,60 @@ type Props = {
   renderText :(currentLanguage :string) => string;
 }
 
-class AboutPage extends React.Component<Props> {
+const AboutPage = ({ renderText } :Props) => {
 
-  render() {
-    const { renderText } = this.props;
+  const trackClick = (link, title) => trackLinkClick(link, title);
 
-    return (
-      <ContentOuterWrapper>
-        <Wrapper>
-          <Typography variant="h1">{renderText(LABELS.RESOURCES)}</Typography>
-          <Description variant="body1">{renderText(LABELS.RESOURCES_DESCRIPTIONS)}</Description>
-          <TextLink></TextLink>
-          <Typography variant="subtitle1">California Resource and Referral Network</Typography>
-          <TextLink></TextLink>
-          <Typography variant="subtitle1">California Covid-19 Childcare Response</Typography>
-          <TextLink></TextLink>
-          <Typography variant="subtitle1">California Parent and Family Resources</Typography>
-          <TextLink></TextLink>
-          <Typography variant="subtitle1">Family Child Care Home Licensing Information</Typography>
-        </Wrapper>
-      </ContentOuterWrapper>
-    );
-  }
+  return (
+    <ContentOuterWrapper>
+      <Wrapper>
+        <Typography variant="h1">{renderText(LABELS.RESOURCES)}</Typography>
+        <Description variant="body1">{renderText(LABELS.RESOURCES_DESCRIPTIONS)}</Description>
+        <Typography variant="subtitle1">{CALIFORNIA_RESOURCE_AND_REFERRAL_NETWORK}</Typography>
+        <a
+            onClick={() => trackClick(
+              RESOURCES[CALIFORNIA_RESOURCE_AND_REFERRAL_NETWORK],
+              CALIFORNIA_RESOURCE_AND_REFERRAL_NETWORK
+            )}
+            href={`tel:${RESOURCES[CALIFORNIA_RESOURCE_AND_REFERRAL_NETWORK]}`}>
+          {RESOURCES[CALIFORNIA_RESOURCE_AND_REFERRAL_NETWORK]}
+        </a>
+        <Typography variant="subtitle1">{CALIFORNIA_COVID19_CHILDCARE_RESPONSE}</Typography>
+        <a
+            onClick={() => trackClick(
+              RESOURCES[CALIFORNIA_COVID19_CHILDCARE_RESPONSE],
+              CALIFORNIA_COVID19_CHILDCARE_RESPONSE
+            )}
+            key={RESOURCES[CALIFORNIA_COVID19_CHILDCARE_RESPONSE]}
+            href={RESOURCES[CALIFORNIA_COVID19_CHILDCARE_RESPONSE]}
+            target="_blank">
+          {RESOURCES[CALIFORNIA_COVID19_CHILDCARE_RESPONSE]}
+        </a>
+        <Typography variant="subtitle1">{CALIFORNIA_PARENT_AND_FAMILY_RESOURCES}</Typography>
+        <a
+            onClick={() => trackClick(
+              RESOURCES[CALIFORNIA_PARENT_AND_FAMILY_RESOURCES],
+              CALIFORNIA_PARENT_AND_FAMILY_RESOURCES
+            )}
+            key={RESOURCES[CALIFORNIA_PARENT_AND_FAMILY_RESOURCES]}
+            href={RESOURCES[CALIFORNIA_PARENT_AND_FAMILY_RESOURCES]}
+            target="_blank">
+          {RESOURCES[CALIFORNIA_PARENT_AND_FAMILY_RESOURCES]}
+        </a>
+        <Typography variant="subtitle1">{FAMILY_CHILD_CARE_HOME_LICENSING_INFORMATION}</Typography>
+        <a
+            onClick={() => trackClick(
+              RESOURCES[FAMILY_CHILD_CARE_HOME_LICENSING_INFORMATION],
+              FAMILY_CHILD_CARE_HOME_LICENSING_INFORMATION
+            )}
+            key={RESOURCES[FAMILY_CHILD_CARE_HOME_LICENSING_INFORMATION]}
+            href={RESOURCES[FAMILY_CHILD_CARE_HOME_LICENSING_INFORMATION]}
+            target="_blank">
+          {RESOURCES[FAMILY_CHILD_CARE_HOME_LICENSING_INFORMATION]}
+        </a>
+      </Wrapper>
+    </ContentOuterWrapper>
+  );
 }
 
 function mapStateToProps(state) {
