61a33fb621ea56bfaaec55a02884758fc6afb9f1
Todd Bergman <todd@openlattice.com>
2020-11-13 18:00:47 -0800

    move components to separate files to be shared and imported

--- src/containers/location/providers/ReferralAgencyHeaderContainer.js
+++ src/containers/location/providers/ReferralAgencyHeaderContainer.js
@@ -6,109 +6,26 @@ import styled from 'styled-components';
 import { faChevronLeft } from '@fortawesome/pro-light-svg-icons';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { Colors } from 'lattice-ui-kit';
-import { useSelector, useDispatch } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
 
-import { selectReferralAgency } from '../LocationsActions';
+import BackButton from '../../../components/controls/BackButton';
 import { ContentOuterWrapper, ContentWrapper } from '../../../components/layout';
 import { HEADER_HEIGHT, HEIGHTS } from '../../../core/style/Sizes';
 import { getTextFnFromState } from '../../../utils/AppUtils';
 import { renderFacilityName } from '../../../utils/DataUtils';
 import { LABELS } from '../../../utils/constants/Labels';
 import { PROVIDERS, STATE } from '../../../utils/constants/StateConstants';
+import { selectReferralAgency } from '../LocationsActions';
+import {
+  Header,
+  StyledHeaderOuterWrapper,
+  StyledHeaderWrapper,
+  SubHeader
+} from '../../styled';
 
 const { LOCATIONS } = STATE;
 
 const { NEUTRAL, PURPLE } = Colors;
-
-const StyledContentOuterWrapper = styled(ContentOuterWrapper)`
-  position: fixed;
-  top: ${HEADER_HEIGHT}px;
-  z-index: 1;
-`;
-
-const StyledContentWrapper = styled(ContentWrapper)`
-  background-color: white;
-  position: relative;
-
-  @media only screen and (min-height: ${HEIGHTS[0]}px) {
-    padding: 10px 25px;
-  }
-
-  @media only screen and (min-height: ${HEIGHTS[1]}px) {
-    padding: 25px;
-  }
-`;
-
-const BackButton = styled.div`
-  align-items: center;
-  color: ${PURPLE.P300};
-  display: flex;
-  flex-direction: row;
-  font-size: 14px;
-  font-weight: 600;
-  text-decoration: none;
-
-  :hover {
-    text-decoration: underline;
-  }
-
-  span {
-    margin-left: 15px;
-  }
-
-  &:hover {
-    cursor: pointer;
-  }
-`;
-
-const Header = styled.div`
-  align-items: center;
-  color: ${NEUTRAL.N700};
-  display: flex;
-  flex-direction: row;
-  font-style: normal;
-  justify-content: space-between;
-
-  @media only screen and (min-height: ${HEIGHTS[0]}px) {
-    padding: 10px 0;
-  }
-
-  @media only screen and (min-height: ${HEIGHTS[1]}px) {
-    padding: 15px 0;
-  }
-
-  div {
-    @media only screen and (min-height: ${HEIGHTS[0]}px) {
-      font-size: 18px;
-      line-height: 14px;
-    }
-
-    @media only screen and (min-height: ${HEIGHTS[1]}px) {
-      font-size: 22px;
-      line-height: 27px;
-    }
-
-    font-weight: 600;
-  }
-
-  span {
-    font-size: 14px;
-    font-weight: normal;
-    line-height: 17px;
-    min-width: fit-content;
-  }
-`;
-
-const SubHeader = styled.div`
-  color: ${NEUTRAL.N700};
-  display: flex;
-  font-size: 14px;
-  font-style: normal;
-  font-weight: normal;
-  justify-content: space-between;
-  line-height: 17px;
-  margin: 3px 0;
-`;
 
 const ReferralAgencyHeaderContainer = () => {
   const getText = useSelector(getTextFnFromState);
@@ -130,8 +47,8 @@ const ReferralAgencyHeaderContainer = () => {
   const typeLabel = getText(LABELS.RESOURCE_AND_REFERRAL);
 
   return (
-    <StyledContentOuterWrapper>
-      <StyledContentWrapper padding="25px">
+    <StyledHeaderOuterWrapper>
+      <StyledHeaderWrapper padding="25px">
         <BackButton onClick={unselectReferralAgency}>
           <FontAwesomeIcon icon={faChevronLeft} />
           <span>{selectedProviderName}</span>
@@ -140,8 +57,8 @@ const ReferralAgencyHeaderContainer = () => {
           <div>{selectedAgencyName}</div>
         </Header>
         <SubHeader>{typeLabel}</SubHeader>
-      </StyledContentWrapper>
-    </StyledContentOuterWrapper>
+      </StyledHeaderWrapper>
+    </StyledHeaderOuterWrapper>
   );
 };
 
