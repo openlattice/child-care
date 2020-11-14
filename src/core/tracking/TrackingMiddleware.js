02454dee504548f2d91368f8e7a7868d5131a71b
Todd Bergman <todd@openlattice.com>
2020-11-13 18:02:12 -0800

    include selectProvider and selectReferralAgency to matchTrackingAction validation

--- src/core/tracking/TrackingMiddleware.js
+++ src/core/tracking/TrackingMiddleware.js
@@ -8,6 +8,8 @@ import isPlainObject from 'lodash/isPlainObject';
 import { LOCATION_CHANGE } from 'connected-react-router';
 import type { DispatchAPI, MiddlewareAPI } from 'redux';
 
+import { SELECT_PROVIDER, SELECT_REFERRAL_AGENCY } from '../../containers/location/LocationsActions';
+
 type TrackingAction = {
   +type :string;
   tracking ?:Object;
@@ -19,6 +21,8 @@ type Action =
 const matchTrackingAction = (action :TrackingAction) => (
   (isPlainObject(action.tracking) && !isEmpty(action.tracking))
   || (action.type === LOCATION_CHANGE)
+  || (action.type === SELECT_PROVIDER)
+  || (action.type === SELECT_REFERRAL_AGENCY)
 );
 
 export default (handlers :Object) => (
