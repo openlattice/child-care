8b171106f3fea62fd9db0d8f7f1a3d9b60f4622d
Todd Bergman <todd@openlattice.com>
2020-11-13 18:00:26 -0800

    update to shouldShowLocation

--- src/containers/location/providers/FamilyHomeLocationSection.js
+++ src/containers/location/providers/FamilyHomeLocationSection.js
@@ -5,7 +5,7 @@ import React from 'react';
 import { useSelector } from 'react-redux';
 
 import { getTextFnFromState } from '../../../utils/AppUtils';
-import { shouldHideLocation } from '../../../utils/DataUtils';
+import { shouldShowLocation } from '../../../utils/DataUtils';
 import { LABELS } from '../../../utils/constants/Labels';
 import { PROVIDERS, STATE } from '../../../utils/constants/StateConstants';
 import { InfoText, TitleRow } from '../../styled';
@@ -17,11 +17,7 @@ const VacancySection = () => {
   const getText = useSelector(getTextFnFromState);
   const provider = useSelector((store) => store.getIn([LOCATIONS, SELECTED_PROVIDER]));
 
-  if (!shouldHideLocation(provider)) {
-    return null;
-  }
-
-  return (
+  return shouldShowLocation(provider) && (
     <TitleRow>
       <InfoText>{getText(LABELS.CONTACT_RR_FOR_INFO)}</InfoText>
     </TitleRow>
