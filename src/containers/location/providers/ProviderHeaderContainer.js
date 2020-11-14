7a3b3776efd7b5c36777caa324c31521afa22d24
Todd Bergman <todd@openlattice.com>
2020-11-13 17:59:39 -0800

    move component to separate files to be shared and import

--- src/containers/location/providers/ProviderHeaderContainer.js
+++ src/containers/location/providers/ProviderHeaderContainer.js
@@ -11,6 +11,7 @@ import { DateTimeUtils } from 'lattice-utils';
 import { connect } from 'react-redux';
 import { bindActionCreators } from 'redux';
 
+import BackButton from '../../../components/controls/BackButton';
 import * as LocationsActions from '../LocationsActions';
 import { ContentOuterWrapper, ContentWrapper, OpenClosedTag } from '../../../components/layout';
 import {
@@ -31,100 +32,16 @@ import { FACILITY_TYPE_LABELS, LABELS } from '../../../utils/constants/Labels';
 import { PROVIDERS, STATE } from '../../../utils/constants/StateConstants';
 import { getCoordinates } from '../../map/MapUtils';
 import type { Translation } from '../../../types';
+import {
+  Header,
+  StyledHeaderOuterWrapper,
+  StyledHeaderWrapper,
+  SubHeader
+} from '../../styled';
 
 const { formatAsRelative } = DateTimeUtils;
 
 const { NEUTRAL, PURPLE } = Colors;
-
-const StyledContentOuterWrapper = styled(ContentOuterWrapper)`
-  position: fixed;
-  top: ${HEADER_HEIGHT}px;
-  z-index: 1;
-`;
-
-const StyledContentWrapper = styled(ContentWrapper)`
-  background-color: white;
-  position: relative;
-
-  @media only screen and (min-height: ${HEIGHTS[0]}px) {
-    padding: 10px 25px;
-  }
-
-  @media only screen and (min-height: ${HEIGHTS[1]}px) {
-    padding: 25px;
-  }
-`;
-
-const BackButton = styled.div`
-  align-items: center;
-  color: ${PURPLE.P300};
-  display: flex;
-  flex-direction: row;
-  font-size: 14px;
-  font-weight: 600;
-  text-decoration: none;
-
-  :hover {
-    text-decoration: underline;
-  }
-
-  span {
-    margin-left: 15px;
-  }
-
-  &:hover {
-    cursor: pointer;
-  }
-`;
-
-const Header = styled.div`
-  align-items: center;
-  color: ${NEUTRAL.N700};
-  display: flex;
-  flex-direction: row;
-  font-style: normal;
-  justify-content: space-between;
-
-  @media only screen and (min-height: ${HEIGHTS[0]}px) {
-    padding: 10px 0;
-  }
-
-  @media only screen and (min-height: ${HEIGHTS[1]}px) {
-    padding: 15px 0;
-  }
-
-  div {
-    @media only screen and (min-height: ${HEIGHTS[0]}px) {
-      font-size: 18px;
-      line-height: 14px;
-    }
-
-    @media only screen and (min-height: ${HEIGHTS[1]}px) {
-      font-size: 22px;
-      line-height: 27px;
-    }
-
-    font-weight: 600;
-  }
-
-  span {
-    font-size: 14px;
-    font-weight: normal;
-    line-height: 17px;
-    min-width: fit-content;
-  }
-`;
-
-const SubHeader = styled.div`
-  color: ${NEUTRAL.N700};
-  display: flex;
-  font-size: 14px;
-  font-style: normal;
-  font-weight: normal;
-  justify-content: space-between;
-  line-height: 17px;
-  margin: 3px 0;
-`;
 
 type Props = {
   actions :{
@@ -171,8 +88,8 @@ class ProviderHeaderContainer extends React.Component<Props> {
     const distance = this.getDistance();
 
     return (
-      <StyledContentOuterWrapper>
-        <StyledContentWrapper padding="25px">
+      <StyledHeaderOuterWrapper>
+        <StyledHeaderWrapper padding="25px">
           <BackButton onClick={() => actions.selectProvider(false)}>
             <FontAwesomeIcon icon={faChevronLeft} />
             <span>{getText(LABELS.SEARCH_RESULTS)}</span>
@@ -191,8 +108,8 @@ class ProviderHeaderContainer extends React.Component<Props> {
             </OpenClosedTag>
             <span>{`${getText(LABELS.LAST_UPDATED)} ${lastModifiedLabel}`}</span>
           </SubHeader>
-        </StyledContentWrapper>
-      </StyledContentOuterWrapper>
+        </StyledHeaderWrapper>
+      </StyledHeaderOuterWrapper>
     );
   }
 }
