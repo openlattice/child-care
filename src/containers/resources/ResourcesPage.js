83378fb017c1becc7c5701ea24774fc442e42fdd
Todd Bergman <todd@openlattice.com>
2020-11-19 15:02:47 -0800

    refactor how the resources page is rendered with getText Fn

--- src/containers/resources/ResourcesPage.js
+++ src/containers/resources/ResourcesPage.js
@@ -10,15 +10,8 @@ import { useSelector } from 'react-redux';
 import { ContentOuterWrapper, ContentWrapper, TextLink } from '../../components/layout';
 import { trackLinkClick } from '../../utils/AnalyticsUtils';
 import { getTextFnFromState } from '../../utils/AppUtils';
-import { LABELS } from '../../utils/constants/Labels';
-import { RESOURCES, SUBTITLES } from '../../utils/constants/ResourcesConstants';
-
-const {
-  CALIFORNIA_RESOURCE_AND_REFERRAL_NETWORK,
-  CALIFORNIA_COVID19_CHILDCARE_RESPONSE,
-  CALIFORNIA_PARENT_AND_FAMILY_RESOURCES,
-  FAMILY_CHILD_CARE_HOME_LICENSING_INFORMATION,
-} = SUBTITLES;
+import { RESOURCES } from '../../utils/constants/Labels';
+import { RESOURCES_CONTENT } from '../../utils/constants/ResourcesConstants';
 
 const Wrapper = styled(ContentWrapper)`
   padding: 30px !important;
@@ -29,63 +22,53 @@ const Wrapper = styled(ContentWrapper)`
   }
 `;
 
+const trackClick = (link, title) => trackLinkClick(link, title);
+const getResourcesContent = (getText) => Object.entries(RESOURCES).map(([key, value]) => (
+  RESOURCES_CONTENT[key] && (
+    <>
+      <Typography variant="subtitle1">{getText(value)}</Typography>
+      {
+        RESOURCES_CONTENT[key].PHONE && (
+          <TextLink
+              aria-label={`phone to ${getText(value)}`}
+              href={`tel:${RESOURCES_CONTENT[key].PHONE}`}
+              onClick={() => trackClick(
+                RESOURCES_CONTENT[key].PHONE,
+                getText(value)
+              )}>
+            {RESOURCES_CONTENT[key].PHONE}
+          </TextLink>
+        )
+      }
+      {
+        RESOURCES_CONTENT[key].URL && (
+          <TextLink
+              aria-label={`link to ${getText(value)}`}
+              href={RESOURCES_CONTENT[key].URL}
+              key={RESOURCES_CONTENT[key].URL}
+              onClick={() => trackClick(
+                RESOURCES_CONTENT[key].URL,
+                getText(value)
+              )}
+              target="_blank">
+            {RESOURCES_CONTENT[key].URL}
+          </TextLink>
+        )
+      }
+    </>
+  )
+));
+
 const ResourcesPage = () => {
 
   const getText = useSelector(getTextFnFromState);
 
-  const trackClick = (link, title) => trackLinkClick(link, title);
-
   return (
     <ContentOuterWrapper>
       <Wrapper>
-        <Typography variant="h1">{getText(LABELS.RESOURCES)}</Typography>
-        <Typography color="textSecondary" variant="body1">{getText(LABELS.RESOURCES_DESCRIPTIONS)}</Typography>
-        <Typography variant="subtitle1">{CALIFORNIA_RESOURCE_AND_REFERRAL_NETWORK}</Typography>
-        <TextLink
-            aria-label={`phone to ${CALIFORNIA_RESOURCE_AND_REFERRAL_NETWORK}`}
-            href={`tel:${RESOURCES[CALIFORNIA_RESOURCE_AND_REFERRAL_NETWORK]}`}
-            onClick={() => trackClick(
-              RESOURCES[CALIFORNIA_RESOURCE_AND_REFERRAL_NETWORK],
-              CALIFORNIA_RESOURCE_AND_REFERRAL_NETWORK
-            )}>
-          {RESOURCES[CALIFORNIA_RESOURCE_AND_REFERRAL_NETWORK]}
-        </TextLink>
-        <Typography variant="subtitle1">{CALIFORNIA_COVID19_CHILDCARE_RESPONSE}</Typography>
-        <TextLink
-            aria-label={`link to ${CALIFORNIA_COVID19_CHILDCARE_RESPONSE}`}
-            href={RESOURCES[CALIFORNIA_COVID19_CHILDCARE_RESPONSE]}
-            key={RESOURCES[CALIFORNIA_COVID19_CHILDCARE_RESPONSE]}
-            onClick={() => trackClick(
-              RESOURCES[CALIFORNIA_COVID19_CHILDCARE_RESPONSE],
-              CALIFORNIA_COVID19_CHILDCARE_RESPONSE
-            )}
-            target="_blank">
-          {RESOURCES[CALIFORNIA_COVID19_CHILDCARE_RESPONSE]}
-        </TextLink>
-        <Typography variant="subtitle1">{CALIFORNIA_PARENT_AND_FAMILY_RESOURCES}</Typography>
-        <TextLink
-            aria-label={`link to ${CALIFORNIA_PARENT_AND_FAMILY_RESOURCES}`}
-            href={RESOURCES[CALIFORNIA_PARENT_AND_FAMILY_RESOURCES]}
-            key={RESOURCES[CALIFORNIA_PARENT_AND_FAMILY_RESOURCES]}
-            onClick={() => trackClick(
-              RESOURCES[CALIFORNIA_PARENT_AND_FAMILY_RESOURCES],
-              CALIFORNIA_PARENT_AND_FAMILY_RESOURCES
-            )}
-            target="_blank">
-          {RESOURCES[CALIFORNIA_PARENT_AND_FAMILY_RESOURCES]}
-        </TextLink>
-        <Typography variant="subtitle1">{FAMILY_CHILD_CARE_HOME_LICENSING_INFORMATION}</Typography>
-        <TextLink
-            aria-label={`link to ${FAMILY_CHILD_CARE_HOME_LICENSING_INFORMATION}`}
-            href={RESOURCES[FAMILY_CHILD_CARE_HOME_LICENSING_INFORMATION]}
-            key={RESOURCES[FAMILY_CHILD_CARE_HOME_LICENSING_INFORMATION]}
-            onClick={() => trackClick(
-              RESOURCES[FAMILY_CHILD_CARE_HOME_LICENSING_INFORMATION],
-              FAMILY_CHILD_CARE_HOME_LICENSING_INFORMATION
-            )}
-            target="_blank">
-          {RESOURCES[FAMILY_CHILD_CARE_HOME_LICENSING_INFORMATION]}
-        </TextLink>
+        <Typography variant="h1">{getText(RESOURCES.RESOURCES)}</Typography>
+        <Typography color="textSecondary" variant="body1">{getText(RESOURCES.RESOURCES_DESCRIPTIONS)}</Typography>
+        { getResourcesContent(getText) }
       </Wrapper>
     </ContentOuterWrapper>
   );
