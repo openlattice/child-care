63ef66c1eaa2f8a775f588ba8002bdd1553fec6a
Todd Bergman <todd@openlattice.com>
2020-11-19 22:26:48 -0800

    add new handlers to index.js

--- src/core/tracking/google/trackinghandlers/index.js
+++ src/core/tracking/google/trackinghandlers/index.js
@@ -4,13 +4,24 @@
 
 import { LOCATION_CHANGE } from 'connected-react-router';
 
-import { SELECT_PROVIDER, SELECT_REFERRAL_AGENCY } from '../../../../containers/location/LocationsActions';
+import {
+  GET_GEO_OPTIONS,
+  SEARCH_LOCATIONS,
+  SEARCH_REFERRAL_AGENCIES,
+  SELECT_PROVIDER,
+  SELECT_REFERRAL_AGENCY
+} from '../../../../containers/location/LocationsActions';
+import GeocodeAddress from './GeocodeAddress';
 import RouteChange from './RouteChange';
+import Search from './Search';
 import ViewProvider from './ViewProviderDetails';
 import ViewReferralAgency from './ViewReferralAgencyDetails';
 
 export default {
+  [GET_GEO_OPTIONS]: GeocodeAddress,
   [LOCATION_CHANGE]: RouteChange,
+  [SEARCH_LOCATIONS]: Search,
+  [SEARCH_REFERRAL_AGENCIES]: Search,
   [SELECT_PROVIDER]: ViewProvider,
-  [SELECT_REFERRAL_AGENCY]: ViewReferralAgency
+  [SELECT_REFERRAL_AGENCY]: ViewReferralAgency,
 };
