cdc6bea07c88a6dd705032f729d0b49d601b48a5
Todd Bergman <todd@openlattice.com>
2020-12-15 17:47:09 -0800

    update no results label object to include without zip scenario

--- src/utils/constants/labels/NoResults.js
+++ src/utils/constants/labels/NoResults.js
@@ -5,10 +5,20 @@
 import { en, es } from './Languages';
 
 export const NO_RESULTS :Object = {
-  DETAILS: {
+  DETAILS_WITH_ZIP: {
     [en]: 'We couldn’t find any childcare facilities within these search parameters. Please refine your search or '
     + 'contact your local Resource & Referral agency:',
     [es]: 'No pudimos encontrar ninguna guardería dentro de estos parámetros de búsqueda. Refine su búsqueda o '
     + 'comuníquese con su agencia local de Recursos y Referencias:'
+  },
+  DETAILS_WITHOUT_ZIP: {
+    [en]: 'We couldn’t find any childcare facilities within these search parameters. Please refine your search or '
+    + 'contact your local Resource & Referral agency. Listed below are referral agencies based on their physical '
+    + 'distance from the location you searched. To yeild more accurate results for agencies serving your area, '
+    + 'include your zip code in your search.',
+    [es]: 'No pudimos encontrar ninguna guardería dentro de estos parámetros de búsqueda. Refine su búsqueda o '
+    + 'comuníquese con su agencia local de Recursos y Referencias. A continuación se enumeran las agencias de '
+    + 'referencia según su distancia física desde la ubicación que buscó. Para obtener resultados más precisos '
+    + 'para las agencias que prestan servicios en su área, incluya su código postal en su búsqueda.'
   },
 };
