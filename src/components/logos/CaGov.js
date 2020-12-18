b4369f438ffedf18be97b91512c0a0eba0afaef2
Todd Bergman <todd@openlattice.com>
2020-12-17 16:22:05 -0800

    added img of ca.gov svg logo to logos folder in components

---
+++ src/components/logos/CaGov.js
@@ -0,0 +1,13 @@
+import styled from 'styled-components';
+
+import calLogo from '../../assets/svg/calIcon.svg';
+
+const CalGov = styled.img.attrs({
+  alt: 'cal.gov',
+  src: calLogo
+})`
+  height: 34px;
+  width: 34px;
+`;
+
+export default CalGov;
