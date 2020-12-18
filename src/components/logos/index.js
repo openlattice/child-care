136831d4f4c080bfbf691ae49a734f3b858a42ed
Todd Bergman <todd@openlattice.com>
2020-12-17 16:22:48 -0800

    created index file for logos folder

---
+++ src/components/logos/index.js
@@ -0,0 +1,11 @@
+import CaGov from './CaGov';
+import CaGovLink from './CaGovLink';
+import CDSS from './CDSS';
+import CDSSLink from './CDSSLink';
+
+export {
+  CaGov,
+  CaGovLink,
+  CDSS,
+  CDSSLink
+};
