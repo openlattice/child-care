8b489d0a0553f8e2f545baa747287c9be0a3169c
Todd Bergman <todd@openlattice.com>
2020-12-17 16:21:43 -0800

    added img of cdss svg logo to logos folder in components

---
+++ src/components/logos/CDSS.js
@@ -0,0 +1,13 @@
+import styled from 'styled-components';
+
+import cdssLogo from '../../assets/svg/cdssLogo.svg';
+
+const CDSS = styled.img.attrs({
+  alt: 'cal.gov',
+  src: cdssLogo
+})`
+  height: 34px;
+  width: 136px;
+`;
+
+export default CDSS;
