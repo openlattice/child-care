a9f4a82fb978b7c09b8a0815014897e09cfc44e3
Todd Bergman <todd@openlattice.com>
2020-12-18 15:30:57 -0800

    update strings

--- src/utils/constants/labels/NoResults.js
+++ src/utils/constants/labels/NoResults.js
@@ -5,20 +5,14 @@
 import { en, es } from './Languages';
 
 export const NO_RESULTS :Object = {
-  DETAILS_WITH_ZIP: {
+  DETAILS: {
     [en]: 'We couldn’t find any childcare facilities within these search parameters. Please refine your search or '
-    + 'contact your local Resource & Referral agency:',
+    + 'contact one of the following Resource & Referral agencies near you:',
     [es]: 'No pudimos encontrar ninguna guardería dentro de estos parámetros de búsqueda. Refine su búsqueda o '
-    + 'comuníquese con su agencia local de Recursos y Referencias:'
+    + 'comuníquese con una de las siguientes agencias de recursos y referencias cercanas a usted:'
   },
   DETAILS_WITHOUT_ZIP: {
-    [en]: 'We couldn’t find any childcare facilities within these search parameters. Please refine your search or '
-    + 'contact your local Resource & Referral agency. Listed below are referral agencies based on their physical '
-    + 'distance from the location you searched. To yeild more accurate results for agencies serving your area, '
-    + 'include your zip code in your search.',
-    [es]: 'No pudimos encontrar ninguna guardería dentro de estos parámetros de búsqueda. Refine su búsqueda o '
-    + 'comuníquese con su agencia local de Recursos y Referencias. A continuación se enumeran las agencias de '
-    + 'referencia según su distancia física desde la ubicación que buscó. Para obtener resultados más precisos '
-    + 'para las agencias que prestan servicios en su área, incluya su código postal en su búsqueda.'
+    [en]: 'For agencies that serve a different location, please include a zip code in your search.',
+    [es]: 'Para las agencias que prestan servicios en una ubicación diferente, incluya un código postal en su búsqueda.'
   },
 };
