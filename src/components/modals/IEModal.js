22ce3b54952740eb49ba72f61e325f5e3d9b80b6
Todd Bergman <todd@openlattice.com>
2020-12-21 22:10:53 -0800

    make her pretty

--- src/components/modals/IEModal.js
+++ src/components/modals/IEModal.js
@@ -1,6 +1,14 @@
 import React from 'react';
+
 import styled from 'styled-components';
-import { Modal } from 'lattice-ui-kit';
+import {
+  faChrome,
+  faEdge,
+  faFirefoxBrowser,
+  faSafari
+} from '@fortawesome/free-brands-svg-icons';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { Modal, Typography } from 'lattice-ui-kit';
 
 import { UNSUPPORTED_BROWSER } from '../../utils/constants/labels';
 
@@ -11,28 +19,40 @@ const Content = styled.div`
   height: 100%;
   justify-content: center;
   min-height: 200px;
-  padding: 10px 0;
+  padding: 30px 40px;
   width: 100%;
 `;
 
-const Text = styled.div`
-  padding: 20px 0;
+const SupportedBroswers = styled.div`
+  align-items: center;
+  display: flex;
+  justify-content: center;
+  margin-top: 20px;
+  width: 100%;
 
-  &:first-child {
-    font-weight: 600;
-    text-align: center;
+  svg {
+    margin: 0 5px;
   }
 `;
 
 const IEModal = ({ getText }) => (
   <Modal withHeader={false} isVisible>
     <Content>
-      <Text>
+      <Typography variant="h1">
         {getText(UNSUPPORTED_BROWSER.HEADER)}
-      </Text>
-      <Text>
+      </Typography>
+      <Typography variant="body1">
+        {getText(UNSUPPORTED_BROWSER.WARNING)}
+      </Typography>
+      <Typography variant="body1">
         {getText(UNSUPPORTED_BROWSER.SUGGESTION)}
-      </Text>
+      </Typography>
+      <SupportedBroswers>
+        <FontAwesomeIcon icon={faChrome} size="3x" />
+        <FontAwesomeIcon icon={faEdge} size="3x" />
+        <FontAwesomeIcon icon={faFirefoxBrowser} size="3x" />
+        <FontAwesomeIcon icon={faSafari} size="3x" />
+      </SupportedBroswers>
     </Content>
   </Modal>
 );
