702633793aa6c2b5caaf5e14564403fdce4c566d
Todd Bergman <todd@openlattice.com>
2020-12-22 13:31:25 -0800

    export logos

--- src/components/logos/index.js
+++ src/components/logos/index.js
@@ -1,11 +1,19 @@
-import CaGov from './CaGov';
-import CaGovLink from './CaGovLink';
 import CDSS from './CDSS';
 import CDSSLink from './CDSSLink';
+import CaGov from './CaGov';
+import CaGovLink from './CaGovLink';
+import Chrome from './Chrome';
+import Edge from './Edge';
+import Firefox from './Firefox';
+import Safari from './Safari';
 
 export {
   CaGov,
   CaGovLink,
   CDSS,
-  CDSSLink
+  CDSSLink,
+  Chrome,
+  Edge,
+  Firefox,
+  Safari,
 };
