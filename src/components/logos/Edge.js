aa90a98a1f7ca16642f94b0fd24f1c93c1d1e86c
Todd Bergman <todd@openlattice.com>
2021-01-04 09:29:42 -0800

    back to svg

--- src/components/logos/Edge.js
+++ src/components/logos/Edge.js
@@ -1,6 +1,6 @@
 import styled from 'styled-components';
 
-import EdgeLogo from '../../assets/images/edge.png';
+import EdgeLogo from '../../assets/svg/edge.svg';
 
 const Edge = styled.img.attrs({
   alt: 'edge icon',
