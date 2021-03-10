9005274576e9024d09cca1bdab3d726fe0be37c7
Todd Bergman <todd@openlattice.com>
2021-03-10 13:48:13 -0800

    updated resources labels for spanish translations

--- src/utils/constants/labels/Resources.js
+++ src/utils/constants/labels/Resources.js
@@ -11,28 +11,28 @@ export const RESOURCES :Object = {
   },
   RESOURCES_DESCRIPTIONS: {
     [en]: 'If you’re having trouble getting in touch with a care provider or have '
-    + ' a general question, please consider contacting one of the following resources:',
-    [es]: 'Si tiene problemas para ponerse en contacto con un proveedor de atención '
-    + 'médica o tiene una pregunta general, considere comunicarse con uno de los siguientes recursos:'
+    + 'a general question, please consider contacting one of the following resources:',
+    [es]: 'Si usted tiene dificultad para contactar con un proveedor de cuidado, o '
+    + 'tiene una pregunta general, por favor, consulte uno de los siguientes recursos:'
   },
   CALIFORNIA_RESOURCE_AND_REFERRAL_NETWORK: {
     [en]: 'California Resource and Referral Network',
-    [es]: 'California Resource and Referral Network'
+    [es]: 'California Resource and Referral Network (Red de Recursos e Información de California) (inglés)'
   },
   CALIFORNIA_COVID19_CHILDCARE_RESPONSE: {
     [en]: 'California Covid-19 Childcare Response',
-    [es]: 'California Covid-19 Childcare Response'
+    [es]: 'Respuesta del Estado de California ante la COVID-19 – Información para proveedores de cuidado infantil'
   },
   CALIFORNIA_PARENT_AND_FAMILY_RESOURCES: {
     [en]: 'California Parent and Family Resources',
-    [es]: 'California Parent and Family Resources'
+    [es]: 'CDSS – Recursos para padres y familias en California (inglés)'
   },
   FAMILY_CHILD_CARE_HOME_LICENSING_INFORMATION: {
     [en]: 'Family Child Care Home Licensing Information',
-    [es]: 'Family Child Care Home Licensing Information'
+    [es]: 'CDSS - Información sobre el licenciamiento de casas familiares de cuidado infantil (inglés)'
   },
   FAQS: {
     [en]: 'Frequently Asked Questions (FAQ)',
-    [es]: 'Frequently Asked Questions (FAQ)'
-  },
+    [es]: 'Preguntas frecuentes (FAQ)'
+  }
 };
