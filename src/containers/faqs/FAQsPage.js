a9fa384fe2783a1739ac0c7303092f5fae3bfaf3
Todd Bergman <todd@openlattice.com>
2021-06-23 16:44:48 -0700

    add new faq information

--- src/containers/faqs/FAQsPage.js
+++ src/containers/faqs/FAQsPage.js
@@ -7,19 +7,30 @@ import styled from 'styled-components';
 import { Colors, Typography } from 'lattice-ui-kit';
 import { useSelector } from 'react-redux';
 
-import { ContentOuterWrapper, ContentWrapper } from '../../components/layout';
+import { ContentOuterWrapper, ContentWrapper, TextLink } from '../../components/layout';
 import { getTextFnFromState } from '../../utils/AppUtils';
-import { FAQS, Q_AND_A } from '../../utils/constants/labels';
+import { FAQS, Q_AND_A, REGIONAL_OFFICE_CONTACTS_URL } from '../../utils/constants/labels';
+import { trackLinkClick } from '../../utils/AnalyticsUtils';
 
 const { BLUE } = Colors;
+
+const trackClick = (link, title) => trackLinkClick(link, title);
 
 const Wrapper = styled(ContentWrapper)`
   background-color: white;
   padding: 30px 30px 60px !important;
 
-  h6 {
+  h5 {
     margin: 20px 0 5px;
   }
+`;
+
+const TextLinkWithMargin = styled(TextLink)`
+  margin-bottom: 10px;
+`;
+
+const WhiteSpaceWrapper = styled(Typography)`
+  white-space: pre-wrap;
 `;
 
 const SubTitle = styled(Typography)`
@@ -35,10 +46,14 @@ const Answer = styled(Typography)`
   margin-bottom: 16px;
 `;
 
+const ListAnswer = styled.ul`
+  margin: 5px 0 10px;
+`;
+
 const getQAndAs = (getText) => Object.entries(FAQS).map(([key, value]) => (
   Q_AND_A[key] && (
     <>
-      <SubTitle variant="h6">{getText(value)}</SubTitle>
+      <SubTitle variant="h5">{getText(value)}</SubTitle>
       {
         Q_AND_A[key].map(({ Q, A }) => (
           <>
@@ -58,8 +73,35 @@ const FAQsPage = () => {
   return (
     <ContentOuterWrapper>
       <Wrapper>
-        <Typography variant="h3">{getText(FAQS.FAQS_TITLE)}</Typography>
-        <Typography color="textSecondary" variant="body1">{getText(FAQS.FAQS_SUBTITLE)}</Typography>
+        <Typography paragraph variant="h3">{getText(FAQS.FAQS_TITLE)}</Typography>
+        <Typography paragraph color="textSecondary" variant="body1">{getText(FAQS.FAQS_SUBTITLE)}</Typography>
+        <SubTitle paragraph variant="h5">{getText(FAQS.COMPLAINT_HOTLINE_TITLE)}</SubTitle>
+        <WhiteSpaceWrapper paragraph variant="body1">{getText(FAQS.COMPLAINT_HOTLINE_SUBTITLE)}</WhiteSpaceWrapper>
+        <WhiteSpaceWrapper paragraph variant="subtitle1">{getText(FAQS.COMPLAINT_HOTLINE_CONTACT)}</WhiteSpaceWrapper>
+        <Typography paragraph variant="body1">{getText(FAQS.COMPLAINT_HOTLINE_BLOCK_1)}</Typography>
+        <Question variant="body2">{getText(FAQS.COMPLAINT_HOTLINE_QUESTION_1)}</Question>
+        <ListAnswer>
+          {
+            getText(FAQS.COMPLAINT_HOTLINE_ANSWER_1).map(((answer) => <li>{answer}</li>))
+          }
+        </ListAnswer>
+        <Typography paragraph variant="body1">{getText(FAQS.COMPLAINT_HOTLINE_BLOCK_2)}</Typography>
+        <TextLinkWithMargin
+            aria-label={`link to ${REGIONAL_OFFICE_CONTACTS_URL}`}
+            href={REGIONAL_OFFICE_CONTACTS_URL}
+            onClick={() => trackClick(
+              REGIONAL_OFFICE_CONTACTS_URL,
+              getText(FAQS.REGIONAL_OFFICE_CONTACTS)
+            )}
+            target="_blank">
+          {getText(FAQS.REGIONAL_OFFICE_CONTACTS)}
+        </TextLinkWithMargin>
+        <Question variant="body2">{getText(FAQS.COMPLAINT_HOTLINE_QUESTION_2)}</Question>
+        <ListAnswer>
+          {
+            getText(FAQS.COMPLAINT_HOTLINE_ANSWER_2).map(((answer) => <li>{answer}</li>))
+          }
+        </ListAnswer>
         { getQAndAs(getText) }
       </Wrapper>
     </ContentOuterWrapper>
